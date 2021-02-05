04d96f7b35c0b6bc87edfa4f6f7321552be23cd3
Archana <diyaarchu1995@gmail.com>
2021-02-05 15:06:58 +0530

    All categories xml

--- FlipkartClone/app/src/main/java/com/nero/flipkart/Adapters/MainAdapter.java
+++ FlipkartClone/app/src/main/java/com/nero/flipkart/Adapters/MainAdapter.java
@@ -1,4 +1,11 @@
 package com.nero.flipkart.Adapters;
 
-public class MainAdapter {
+import static com.nero.flipkart.Model.FashionClass.FASHION_BLOGS_LAYOUT;
+import static com.nero.flipkart.Model.FashionClass.FASHION_SALES_LAYOUT;
+
+
+public class MainAdapter  {
+
+
+
 }
